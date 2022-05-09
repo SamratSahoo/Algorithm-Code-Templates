b123cb03a078b21ca30a98fb980b228f56d30fcf
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2022-05-09 17:00:05 -0500

    added slow and fast pointer

--- fast_and_slow_pointer.py
+++ fast_and_slow_pointer.py
@@ -1,2 +1,27 @@
+# Detect cycle in Linked List: Turtoise and Hare Algorithm
 def fast_and_slow_pointer(head):
-    pass
+    # Null Case
+    if not head:
+        return False 
+    
+    # Instantiate two nodes, one that goes fast, one that goes slow
+    slow = head
+    fast = head
+
+    # Check if fast.next is not Null and fast.next.next is not null
+    while fast.next and fast.next.next:
+        
+        # Slow pointer will go one forward
+        slow = slow.next
+
+        # Fast pointer will go two forward
+        fast = fast.next.next
+
+        # If slow and fast are equal
+        if slow == fast:
+
+            # There is a loop in the linked list
+            return True
+
+    # Else there is not a loop in the linked list
+    return False
