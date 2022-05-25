4a6bd771df16df6319924b7ae8734f8608c22620
pshah413 <65915193+pshah413@users.noreply.github.com>
2022-05-25 16:50:37 -0400

    graphs

---
+++ bfsgraph.py
@@ -0,0 +1,18 @@
+def validPath(self, n, edges, start, destination):
+    neighbors = defaultdict(list)
+    for n1, n2 in edges:
+        neighbors[n1].append(n2)
+        neighbors[n2].append(n1)
+        
+    q = deque([start])
+    seen = set([start])
+    while q:
+        node = q.popleft()            
+        if node == destination:
+            return True            
+        for n in neighbors[node]:
+            if n not in seen:
+                seen.add(n)
+                q.append(n)
+            
+    return False
