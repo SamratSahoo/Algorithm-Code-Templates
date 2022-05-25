2790c7a0e01ac1ea87cd6aa5b995f2e6a39d4903
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2022-05-25 17:36:09 -0500

    added matrix dfs

---
+++ matrix_dfs.py
@@ -0,0 +1,20 @@
+def matrix_dfs(matrix):
+    
+    def dfs(i, j):
+        if not 0 <= i < len(matrix) or not 0 <= j < len(matrix[0]) or matrix[i][j] != "1":
+            return
+
+        matrix[i][j] = "."
+        dfs(i+1, j)
+        dfs(i-1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
+
+    count = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == "1":
+                dfs(i,j)
+                count += 1
+
+    return count
